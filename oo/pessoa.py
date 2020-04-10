0d47284e4e8a0ea8b93833f6fd01808cf5bdac62
MichelSantosNaki <60993267+MichelSantosNaki@users.noreply.github.com>
2020-04-10 19:30:55 -0300

    criando e modificando atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None): # atributos
+        self.nome=nome
     def cumprimentar(self):
-        return f'Olá Mundo {id(self)}' # se verificar o id de self é o mesmo do p!
+        return f'Olá Mundo {self.nome}' # se verificar o id de self é o mesmo do p!
 
 if __name__ == '__main__':
-    p= Pessoa()
+    p= Pessoa('Teste') # chama a classe pessoas mandando  a str Teste para nome
+    print(p.cumprimentar() )
+    #mudando atributos
+    p.nome='Michel' # agora atribuindo a str Michel
     print(p.cumprimentar())
-    print(id(p))
 
 
 
