46b826011ebe36075ec32f6bd4bbab466dc268ec
MichelSantosNaki <60993267+MichelSantosNaki@users.noreply.github.com>
2020-04-10 19:03:06 -0300

    criando metodos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,11 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá Mundo {id(self)}' # se verificar o id de self é o mesmo do p!
+
+if __name__ == '__main__':
+    p= Pessoa()
+    print(p.cumprimentar())
+    print(id(p))
 
 
 
